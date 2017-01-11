23ed335e20a403a4874d3b47337abe63291b8226
Jared Koumentis <jared@koumentis.com>
2017-01-11 16:01:41 -0700

    WIP

--- lib/resque/tasks.rb
+++ lib/resque/tasks.rb
@@ -41,7 +41,7 @@ namespace :resque do
   task :preload => :setup do
     if defined?(Rails)
       if Rails::VERSION::MAJOR > 4
-        puts "do nothing"
+        Rails.application.config.eager_load_namespaces.each(&:eager_load!)
 
       elsif Rails::VERSION::MAJOR > 3
         ActiveSupport.run_load_hooks(:before_eager_load, Rails.application)
